185578ae9c23f1681cf3bf7613247e5f69f67c0e
Gracianna27 <graciannaditche27@gmail.com>
2025-09-25 08:40:39 +0800

    Added Factorial

--- hello.js
+++ hello.js
@@ -7,3 +7,8 @@ sum = num1 + num2;
 diff = num1 - num2;
 prod = num1 * num2;
 quo = num1 / num2;
+
+console.log("The sum of" + num1 +" & " + num2 +" is " + sum);
+console.log("The diffence of " + num1 +" & " + num2 +"is" + diff);
+console.log("The product of" + num1 +"& " + num2 +" is" + prod);
+console.log("The quotient of" + num1 + "&" + num2 +"is " + quo);
